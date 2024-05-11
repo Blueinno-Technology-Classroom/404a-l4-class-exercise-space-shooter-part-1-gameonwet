8d0f3ad655f46ce6936412d1ed6de2c020d483bc
hi <calebcheung1223@gmail.com>
2024-05-11 12:14:43 +0800

    LESON 5 DF

--- main.py
+++ main.py
@@ -1,72 +1,91 @@
 import pgzrun
 import random
+from pgzhelper import *
 
 WIDTH = 1024
 HEIGHT = 768
 
 player = Actor("playership1_blue")
 player.pos = (WIDTH / 2, HEIGHT / 2)
-
+player.hp = 100
 enemies = []
 lasers = []
 elasers = []
 
 def update():
-    if random.randint(0, 100) < 2:
-        enemy = Actor("enemyred2")
-        enemy.x = random.randint(0, WIDTH)
-        enemies.append(enemy)
-    
+    if player.hp > 0:
+        if random.randint(0, 100) < 2:
+            enemy = Actor("enemyred2")
+            enemy.x = random.randint(0, WIDTH)
+            enemy.point_towards(player)
+            enemies.append(enemy)
+        
 
-    for e in enemies:
-        if random.randint(0, 100) <20:
-            elaser = Actor('laserred07')
-            elaser.pos = e.pos
-            elasers.append(elaser)
-
-    if keyboard.space:
-         laser = Actor('laserblue01')
-         laser.pos = player.pos
-         lasers.append(laser)
-
-
-    if keyboard.a:
-        player.x -= 5
-    if keyboard.d:
-        player.x += 5
-    if keyboard.w:
-        player.y -= 5
-    if keyboard.s:
-        player.y += 5
-
-    if player.left <= 0:
-        player.left = 0
-    if player.right >= WIDTH:
-        player.right = WIDTH
-    if player.top <= 0:
-        player.top = 0
-    if player.bottom >= HEIGHT:
-        player.bottom = HEIGHT
-
-    for l in lasers:
-        l.y -=5
-        if l.bottom < 0:
-            lasers.remove(l)
-            continue
         for e in enemies:
-            if l.colliderect(e):
-                lasers.remove(l)
+            e.move_forward(2)
+            if player.colliderect(e):
+                player.hp -= 5
                 enemies.remove(e)
-                break
+                continue
+            if random.randint(0, 100) <2:
+                elaser = Actor('laserred07')
+                elaser.pos = e.pos
+                elaser.point_towards(player)
+                elasers.append(elaser)
 
-    for el in elasers:
-        el.y += 5
-        if el.top >= HEIGHT:
-            elasers.remove(el)
-            continue
+        if keyboard.space:
+            laser = Actor('laserblue07')
+            laser.pos = player.pos
+            if enemies:
+                laser.point_towards(random.choice(enemies))
+            else:
+                laser.angle = 90
+                lasers.append(laser)
+            lasers.append(laser)
+
+
+        if keyboard.a:
+            player.x -= 5
+        if keyboard.d:
+            player.x += 5
+        if keyboard.w:
+            player.y -= 5
+        if keyboard.s:
+            player.y += 5
+
+        if player.left <= 0:
+            player.left = 0
+        if player.right >= WIDTH:
+            player.right = WIDTH
+        if player.top <= 0:
+            player.top = 0
+        if player.bottom >= HEIGHT:
+            player.bottom = HEIGHT
+
+        for l in lasers:
+            l.move_forward(5)
+            if l.bottom < 0:
+                lasers.remove(l)
+                continue
+            for e in enemies:
+                if l.colliderect(e):
+                    lasers.remove(l)
+                    enemies.remove(e)
+                    break
+
+        for el in elasers:
+            el.move_forward(5)
+            if el.top >= HEIGHT:
+                elasers.remove(el)
+                continue
+            if player.colliderect(el):
+                player.hp -= 1
+                elasers.remove(el)
+                break
 
 def draw():
     screen.clear()
+    screen.draw.filled_rect(Rect((0, 0), (WIDTH * player.hp / 100, 20)), 'green')
     player.draw()
     
     for e in enemies:
