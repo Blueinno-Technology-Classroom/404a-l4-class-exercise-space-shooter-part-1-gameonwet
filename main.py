7d29ac41107cd164f332c9d3ad6fb9234ec28fb0
hi <calebcheung1223@gmail.com>
2024-06-01 12:03:19 +0800

    lesson 8

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pgzrun
 import random
+import math
 from pgzhelper import *
 
 WIDTH = 1024
@@ -8,21 +9,76 @@ HEIGHT = 768
 player = Actor("playership1_blue")
 player.pos = (WIDTH / 2, HEIGHT / 2)
 player.hp = 100
+player.score = 0
+
 enemies = []
+
 lasers = []
 elasers = []
+laser_count = 0
+
+mouse_pos = [0, 0]
 
 def update():
+    global laser_count
     if player.hp > 0:
+
+        player.angle = player.angle_to(mouse_pos) - 90
+
+        # enemy lvl 1
         if random.randint(0, 100) < 2:
             enemy = Actor("enemyred2")
-            enemy.x = random.randint(0, WIDTH)
+            enemy.lv = 1
+            enemy.hp = 1
+
+            from_dir = random.randint(0, 3)
+            if from_dir == 0:
+                enemy.x = random.randint(0, WIDTH)
+                enemy.y = 0
+            elif from_dir == 1:
+                enemy.x = 0
+                enemy.y = random.randint(0, HEIGHT)
+            elif from_dir == 2:
+                enemy.x = random.randint(0, WIDTH)
+                enemy.y = HEIGHT
+            elif from_dir == 3:
+                enemy.x = WIDTH
+                enemy.y = random.randint(0, HEIGHT)
+
+            enemy.point_towards(player)
+            enemies.append(enemy)
+
+        # enemy lvl 2
+        if random.randint(0, 100) < 1 and player.score >= 100:
+            enemy = Actor("enemyred5")
+            enemy.lv = 2
+            enemy.hp = 2
+
+            from_dir = random.randint(0, 3)
+            if from_dir == 0:
+                enemy.x = random.randint(0, WIDTH)
+                enemy.y = 0
+            elif from_dir == 1:
+                enemy.x = 0
+                enemy.y = random.randint(0, HEIGHT)
+            elif from_dir == 2:
+                enemy.x = random.randint(0, WIDTH)
+                enemy.y = HEIGHT
+            elif from_dir == 3:
+                enemy.x = WIDTH
+                enemy.y = random.randint(0, HEIGHT)
+
             enemy.point_towards(player)
             enemies.append(enemy)
         
 
+
+
+
         for e in enemies:
             e.move_forward(2)
+            if e.left > WIDTH or e.right < 0 or e.top > HEIGHT or e.bottom < 0:
+                enemies.remove(e)
             if player.colliderect(e):
                 player.hp -= 5
                 enemies.remove(e)
@@ -34,15 +90,13 @@ def update():
                 elasers.append(elaser)
 
         if keyboard.space:
-            laser = Actor('laserblue07')
-            laser.pos = player.pos
-            if enemies:
-                laser.point_towards(random.choice(enemies))
-            else:
-                laser.angle = 90
+            laser_count += 1
+        if laser_count >= 10:
+                laser = Actor('laserblue07')
+                laser.pos = player.pos
+                laser.angle = player.angle + 90
                 lasers.append(laser)
-            lasers.append(laser)
-
+                laser_count = 0
 
         if keyboard.a:
             player.x -= 5
@@ -63,15 +117,18 @@ def update():
             player.bottom = HEIGHT
 
         for l in lasers:
-            l.move_forward(5)
+            l.move_forward(10)
             if l.bottom < 0:
                 lasers.remove(l)
                 continue
             for e in enemies:
                 if l.colliderect(e):
+                    e.hp -= 1
                     lasers.remove(l)
-                    enemies.remove(e)
-                    break
+                    if e.hp <= 0:
+                        enemies.remove(e)
+                        player.score += 10
+                        break
 
         for el in elasers:
             el.move_forward(5)
@@ -82,10 +139,18 @@ def update():
                 player.hp -= 1
                 elasers.remove(el)
                 break
+    else:
+        player.score = 0
+        player.hp = 100
+
+def on_mouse_move(pos):
+    global mouse_pos
+    mouse_pos = pos
 
 def draw():
     screen.clear()
     screen.draw.filled_rect(Rect((0, 0), (WIDTH * player.hp / 100, 20)), 'green')
+    screen.draw.text(str(player.score), (10, 30), fontsize = 60)
     player.draw()
     
     for e in enemies:
